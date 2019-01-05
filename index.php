ce4c110f5e841319c9e5a4fe053497389cd52ae6
Alejandro González Reyes <phpconestilo@gmail.com>
2019-01-04 20:46:16 -0600

    Funciones del Programador

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   $name = 'Alejandro González Reyes';
 
   //Limit para mostrar las experiencias de trabajo menores o iguales a 20 meses
-  $limiteMeses = 20;
+  $limiteMeses = 40;
   
   $jobs = [
     [
@@ -12,14 +12,14 @@
       'description' => 'Experiencia detallada trabajando con PHP',
       'logros'      => ['Sistema clínico dental', 'Sistema bolsa de trabajo', 'Sistema de punto de venta'],
       'visible'     => true,
-      'meses'       => 8,
+      'meses'       => 1,
     ],
     [
       'title'       => 'Desarrollador Python',
       'description' => 'Narración experiencia con desarrollos utilizando el lenguaje de programación Python',
       'logros'      => ['GUI de escritorio', 'Videojuego de plataformas', 'Plataforma educativa IUEM'],
-      'visible'     => false,
-      'meses'       => 2,
+      'visible'     => true,
+      'meses'       => 7,
     ],
     [
       'title'       => 'Desarrollador FrontEnd',
@@ -36,7 +36,70 @@
       'meses'       => 5,
     ],
   ];
-  
+
+
+  /**
+   * Función que retorna una cadena con el formato requerido para mostrar el tiempo según la experiencia en una determinada tecnología
+   * Recibe como parametro la sumatoria de meses
+   * 
+   * Considera formatos de tiempo en singular y plural, así como la conjunción para unir las leyendes
+   * 1 mes | 2 meses | 1 año | 5 años | 1 año y 2 meses | 5 años y 10 meses
+   */
+  function tiempoLaboral($meses) {
+    //$num_anios = floor($meses / 12);
+    $num_anios = intdiv($meses, 12);
+    $num_meses = $meses % 12;
+    
+    $leyenda = '';
+    $bandera_y = false;
+    
+    if($num_anios > 0) {
+      
+      $bandera_y = true;
+      
+      if($num_anios > 1) 
+        $leyenda = "$num_anios años";
+      else   
+        $leyenda = "$num_anios año";
+    }
+    if($num_meses > 0) {
+      if($num_meses > 1) 
+        $leyenda .= $bandera_y ? " y $num_meses meses" : "$num_meses meses";
+      else   
+        $leyenda .= $bandera_y ? " y $num_meses mes" : "$num_meses mes";
+    }
+    return $leyenda;
+  }
+
+
+  /**
+   * Función para imprimir los detalles de un trabajo
+   * recibe como parametro el trabajo actual y el total de meses de experiencia (sumatoria)
+   */
+  function imprimirDetallesJob($job, $total_meses) {
+
+    /**
+     * Mostrar solo aquellas experiencias de trabajo que en el arreglo estén declaradas
+     * como visibles. En caso contrario...
+     * Indicar que la función debe salir de ejecucion con RETURN
+     */
+    if($job['visible'] != true) {
+      return;
+    }
+
+    echo "<li class='work-position'>";
+      echo "<h5>{$job['title']}</h5>";
+      echo "<p>" . $job['description'] . "</p>";
+      //Invocar otra función cuya labor es mostrar una leyenda formateada total de tiempo de experiencia
+      echo "<p>Hace ya: ". tiempoLaboral($total_meses) ."</p>";
+      echo "<strong>Achievements:</strong>";
+      echo "<ul>";
+        for ($i=0; $i < count($job['logros']); $i++) { 
+          echo "<li>{$job['logros'][$i]}.</li>";
+        }
+      echo "</ul>";
+    echo "</li>";
+  }
 ?>
 
 <!doctype html>
@@ -125,27 +188,9 @@
               if($totalMeses > $limiteMeses) {
                 break;
               }
-
-              /**
-               * Mostrar solo aquellas experiencias de trabajo que en el arreglo estén declaradas
-               * como visibles. En caso contrario...
-               * Indicar al ciclo que CONTINUE con la siguiente iteración (salta o brinca)
-               */
-              if($jobs[$contador]['visible'] != true) {
-                continue;
-              }
               
-              echo "<li class='work-position'>";
-                echo "<h5>{$jobs[$contador]['title']}</h5>";
-                echo "<p>" . $jobs[$contador]['description'] . "</p>";
-                echo "<p>Hace ya: $totalMeses meses</p>";
-                echo "<strong>Achievements:</strong>";
-                echo "<ul>";
-                  for ($i=0; $i < count($jobs[$contador]['logros']); $i++) { 
-                    echo "<li>{$jobs[$contador]['logros'][$i]}.</li>";
-                  }
-                echo "</ul>";
-              echo "</li>";
+              //Invocar la función para que imprima los detalles del trabajo actual en el arreglo, así como la suma total de meses de experiencia
+              imprimirDetallesJob($jobs[$contador], $totalMeses);
             }
             ?>
           </ul>
